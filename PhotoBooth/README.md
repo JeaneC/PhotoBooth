29d08c8e9189b170cb69d451e7b3d1e8c64a9a6e
JeaneC <volbohel@gmail.com>
2017-08-17 00:55:00 -0400

    Commit B
    - Basic UI of the app is designed

--- PhotoBooth/README.md
+++ PhotoBooth/README.md
@@ -1,1 +1,8 @@
-"# photobooth" 
+# photobooth
+
+1. npm install electron --save-dev﻿
+
+
+Helpful Repositories
+1. https://github.com/mosch/react-avatar-editor
+2. https://github.com/fengyuanchen/cropperjs
--- PhotoBooth/app.js
+++ PhotoBooth/app.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
+var Cropper = require('cropperjs')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
